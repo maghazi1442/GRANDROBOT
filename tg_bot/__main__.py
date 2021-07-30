a2b0576a49533a8d6cda3970ea9935e8717e9b14
maghazi1442 <83565356+maghazi1442@users.noreply.github.com>
2021-07-30 07:41:51 +0700

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -40,7 +40,7 @@ Dibuat oleh @joincontact
 
 HELP_STRINGS = """
 
-Hello! my name *{}*.
+Hello! my name *{ANNAJIYAH MEDIA CENTER}*.
 
 *Main* commands available:
  - /start: start the bot
@@ -147,16 +147,16 @@ def start(bot: Bot, update: Update, args: List[str]):
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
                 parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="🤝HELP🤝",
                                                                        callback_data="help_back".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="🧑‍💻My Creator🧑‍💻",
-                                                                       url="t.me/teamishere")],
-                                                                                   [InlineKeyboardButton(text="ADD GRAND OFFICIAL TO YOUR GROUP",
-                                                                       url="t.me/{}?startgroup=true".format(bot.username)),
-                                                                                   InlineKeyboardButton(text="Source Code",
-                                                                       url="https://github.com/legendx22/GRANDROBOT")
+                                                                                   InlineKeyboardButton(text="contact",
+                                                                       url="t.me/joincontact")],
+                                                                                   [InlineKeyboardButton(text="Tambahkan bot ke group",
+                                                                       url="t.me/AMC_00_Bot?startgroup=true".format(bot.username)),
+                                                                                   InlineKeyboardButton(text="channels",
+                                                                       url="https://t.me/joinchat/EGLAkwl9tmk3YWJh")
                                                                                  ]]))
 
     else:
-        update.effective_message.reply_text("Yuss, I am Already ONline")
+        update.effective_message.reply_text("ANNAJIYAH MEDIA CENTER")
 
 
 def send_start(bot, update):
